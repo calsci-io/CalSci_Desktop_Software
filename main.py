6212cde6913c7dda1cc400b7d250f3dd0fede460
shoubhik saha <hellosobik@gmail.com>
2026-01-29 20:45:02 +0530

    app development started

---
+++ main.py
@@ -0,0 +1,27 @@
+import sys
+from PySide6.QtWidgets import QApplication, QMainWindow, QLabel
+from PySide6.QtCore import Qt
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("PySide6 App")
+        self.resize(400, 300)
+
+        label = QLabel("Hello, PySide6!", self)
+        label.setAlignment(Qt.AlignCenter)
+
+        self.setCentralWidget(label)
+
+
+def main():
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
+
+
+if __name__ == "__main__":
+    main()
